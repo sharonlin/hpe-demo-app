3df3708290955a004f0248d0ed0e7ba16ef60203
Sharon Lin <sharon.lin.1@gmail.com>
2017-07-20 13:07:09 +0300

    break testB, Catch me if u can :-))))

--- ui-tests/src/test/java/com/hp/devops/demoapp/tests/ui/TestB.java
+++ ui-tests/src/test/java/com/hp/devops/demoapp/tests/ui/TestB.java
@@ -83,16 +83,17 @@ public class TestB {
 
     @Test
     public void testUIcaseC() {
-        System.out.println("Proudly running test " + Thread.currentThread().getStackTrace()[1]);
-        WebElement query;
-        if(isMusicApp){
-            query = driver.findElement(By.id("bandsList"));
-            Assert.assertEquals(query.getTagName(), "div");
-        } else {
-            query = driver.findElement(By.id("jenkins"));
-            Assert.assertEquals(query.getTagName(), "body");
-        }
-        Assert.assertEquals(query.isDisplayed(), true);
+//         System.out.println("Proudly running test " + Thread.currentThread().getStackTrace()[1]);
+//         WebElement query;
+//         if(isMusicApp){
+//             query = driver.findElement(By.id("bandsList"));
+//             Assert.assertEquals(query.getTagName(), "div");
+//         } else {
+//             query = driver.findElement(By.id("jenkins"));
+//             Assert.assertEquals(query.getTagName(), "body");
+//         }
+//         Assert.assertEquals(query.isDisplayed(), true);
+       Assert.fail("Catch me if u can... :-)))");
     }
 
     @Category(SpecialTests.class)
