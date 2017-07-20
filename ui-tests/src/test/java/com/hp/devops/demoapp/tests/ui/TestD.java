5c2568ee18bd95d28224007e0f1825d6c2cdbce1
Sharon Lin <sharon.lin.1@gmail.com>
2017-07-20 14:56:50 +0300

    Catch me if u can

--- ui-tests/src/test/java/com/hp/devops/demoapp/tests/ui/TestD.java
+++ ui-tests/src/test/java/com/hp/devops/demoapp/tests/ui/TestD.java
@@ -11,7 +11,10 @@ public class TestD {
 
 	@Test
 	public void testUIcaseAlwaysPass() {
+		//Unmark the Assert.fail line and comment the rest of the methods
+		//Assert.fail("Catch me if u can...)))");
 		System.out.println("Proudly  running test " + Thread.currentThread().getStackTrace()[1]);
 		Assert.assertTrue(true);
+		
 	}
 }
